a098c382de772ecc22f888f7c35467722e24e7d7
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2012-07-13 16:12:34 -0400

    create the skeleton of the app

---
+++ application/controllers/link_stat.php
@@ -0,0 +1,15 @@
+<?php
+
+class Link_Stat extends CI_Controller {
+
+	// Show the aggregate statistics
+	public function all() {
+
+	}
+
+	// Show only the statistics for a given link (or
+	// shorturl)
+	public function link_stat() {
+
+	}
+}
