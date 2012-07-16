30bd681b2f8fada0a56d7fe5f4dc215c925b734e
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2012-07-16 14:01:08 -0400

    date picker

--- application/views/link_stats/all.php
+++ application/views/link_stats/all.php
@@ -2,7 +2,9 @@
 <html>
 	<head>
 		<script type="text/javascript" src="<?php echo base_url("js/jquery.js"); ?>"></script>
+		<script type="text/javascript" src="<?php echo base_url("js/jquery-ui.js"); ?>"></script>
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url("css/bootstrap.css"); ?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url("css/black-tie/jquery-ui.custom.css"); ?>" />
 	</head>
 	<body>
 		<div class="container-fluid">
@@ -27,14 +29,29 @@
 				<label class="checkbox">
 					<input id="all" type="checkbox" checked="checked"> Show all
 				</label>
-				<input type="text" class="input-medium search-query">
-				<button type="submit" class="btn">Search</button>
+				<input type="text" id="before-date" class="input-medium">
+				<input type="text" id="after-date" class="input-medium">
+				<button type="submit" class="btn">Select Date</button>
 			</div>
 		</div>
 	</body>
 	<script>
 		$(document).ready(function() {
+			var today = new Date();
 
+			$("#before-date").datepicker({
+				maxDate: today,
+				onSelect: function(selectDate) {
+					$("#after-date").datepicker("option", "minDate", selectDate);
+				}
+			});
+
+			$("#after-date").datepicker({
+				maxDate: today,
+				onSelect: function(selectDate) {
+					$("#before-date").datepicker("option", "maxDate", selectDate);
+				}
+			});
 		});
 	</script>
 </html>
