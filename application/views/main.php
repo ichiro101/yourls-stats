06cfa921bd892e2269d3c693641cd6c160af9f34
Ichiro Yamamoto <ichiyamamoto@gmail.com>
2012-07-16 10:09:33 -0400

    headers

--- application/views/main.php
+++ application/views/main.php
@@ -1,10 +1,21 @@
+<?php 
+	$this->load->helper('url'); 
+?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<script type="text/javascript" src="js/jquery.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<script type="text/javascript" src="<?php echo base_url("js/jquery.js"); ?>"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url("css/bootstrap.css"); ?>" />
 	</head>
 	<body>
-		
+		<div class="container-fluid">
+			<div class="navbar">
+			<div class="navbar-inner">
+				<a class="brand" href="<?php echo site_url('/'); ?>">Yourls Stats</a>
+			<div class="container">
+			</div>
+			</div>
+			</div>
+		</div>
 	</body>
 </html>
